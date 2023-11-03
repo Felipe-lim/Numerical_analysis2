a5dc6887ef4c139f3f5aa1451325e530f3608653
Felipe Lima <felipy0439@gmail.com>
2023-11-03 01:25:32 -0300

    ajustei o 15

--- python_definitivo/15_interpolacao_greg_newton.py
+++ python_definitivo/15_interpolacao_greg_newton.py
@@ -1,12 +1,12 @@
 class Dado:
     # Número de pontos
-    m = 5
+    m = 3
     # Valores de x
-    x = [0.1, 0.3, 0.4, 0.6, 0.7]
+    x = [110, 120, 130]
     # Valores correspondentes de y
-    y = [0.3162, 0.5477, 0.6325, 0.7746, 0.8367]
+    y = [2.0410, 2.0790, 2.1140]
     # Ponto para estimativa
-    z = 0.2
+    z = 115
 
     # Resultado esperado da interpolação
     #resultado = 0.4456
@@ -27,16 +27,17 @@ def inter_greg_new(m, x, y, z):
             # Calcula diferença dividida de ordem k
             dely[i] = (dely[i] - dely[i - 1])
 
-    # Inicializa r com o termo mais à direita da tabela (última diferença dividida)
-    r = dely[m - 1]
     # Calcula o termo u da fórmula de interpolação
     u = (z - x[0]) / (x[1] - x[0])
+    # Inicializa r com o termo mais à direita da tabela (última diferença dividida)
+    r = dely[m - 1]
+
 
     # Loop para calcular o valor interpolado r
-    for i in range(m - 2, -1, -1):
+    for i in range(m - 1, 0, -1):
         # Calcula o próximo termo da série de Gregory-Newton
         # Obs: A expressão original (i + dely[i]) parece estar incorreta, pois as diferenças divididas não devem ser somadas aos índices.
-        r = r * (u - i + 1) / (i + dely[i])  # O comentário indica que esta linha pode conter um erro.
+        r = (r * (u - (i - 1))) / i + dely[i - 1]  # O comentário indica que esta linha pode conter um erro.
 
     # Exibe o resultado da interpolação
     print(r)
